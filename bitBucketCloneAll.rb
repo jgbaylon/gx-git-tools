e1c47addb804395ecbedceae9a02633c0fae69a8
Joseph Baylon <joseph@gradientx.com>
2013-11-27 13:59:30 -0800

    GX-1043: fixed issues with bitBucketCloneAll.rb

--- bitBucketCloneAll.rb
+++ bitBucketCloneAll.rb
@@ -1,26 +1,54 @@
 require 'open-uri'
 require 'rubygems'
+require 'oauth'
 require 'json'
 
 =begin
-1. Follow instructions to generate oauth_token_secret and oauth_token from https://confluence.atlassian.com/display/BITBUCKET/OAuth+on+Bitbucket
-2. Modify BITBUCKET_* environment variables from setEnv.sh; then run
+1. Manage account > Integrated applications > Add consumer. Take note of Key and Secret.
+2. Request token using the OAuth Test Client: http://term.ie/oauth/example/client.php
+   a. endpoint: https://bitbucket.org/api/1.0/oauth/request_token?oauth_callback=http%3A%2F%2Flocalhost
+   b. consumer key: <Key>
+   c. consumer secret: <Secret>
+   d. hit request_token button and open the downloaded request_token file and make sure to have these parameters
+      oauth_token_secret=<oauth token secret>&oauth_token=<oauth token>&oauth_callback_confirmed=true
+3. Redirect user to Bitbucket to authorize application:
+   a. open new window: https://bitbucket.org/api/1.0/oauth/authenticate?oauth_token=<oauth token>
+   b. hit Grant Access button
+   c. from the address bar: take note of oauth_verifier=<oauth verifier>
+4. Request an Access Token:
+   a. go back to OAuth Test Client window
+      i. token: <oauth token>
+      ii. token secret: <oauth token secret>
+      iii. update endpoint: https://bitbucket.org/api/1.0/oauth/access_token?oauth_version=1.0&oauth_verifier=<oauth verifier>
+      iv. hit access_token button and open the downloaded access_token file and make sure to have these parameters
+          oauth_token_secret=<oauth token secret>&oauth_token=<oauth token>      
+5. Modify BITBUCKET_* environment variables from setEnv.sh; then run
+   source setEnv.sh
    ./setEnv.sh
-5. To run
+6. sudo gem install oauth
+7. To run
    ruby bitBucketCloneAll.rb
 =end
 
 system("rm clone_script.sh")
 
-token = ENV['BITBUCKET_OAUTH_TOKEN']
-secret = ENV['BITBUCKET_OAUTH_TOKEN_SECRET']
+consumerKey = ENV['BITBUCKET_CONSUMER_KEY']
+consumerSecret = ENV['BITBUCKET_CONSUMER_SECRET']
+oauthToken = ENV['BITBUCKET_OAUTH_TOKEN']
+oauthTokenSecret = ENV['BITBUCKET_OAUTH_TOKEN_SECRET']
 organization = ENV['BITBUCKET_ORGANIZATION']
 path = ENV['BITBUCKET_LOCAL_PATH']
+
+@consumer = OAuth::Consumer.new consumerKey,
+                                consumerSecret,
+                                {:site=>"https://bitbucket.org"}
+
+@accessToken = OAuth::AccessToken.new(@consumer, oauthToken, oauthTokenSecret)
 
 File.open("clone_script.sh", 'a') do |f|
   f.puts("cd " + path)
 
-  content = open("https://bitbucket.org/api/1.0/user/repositories?oauth_token=" + oauth_token + "&oauth_token_secret=" + oauth_token_secret).read
+  content = @accessToken.get("/api/1.0/user/repositories").body
   json = JSON.parse(content)
 
   json.each do |repo|
