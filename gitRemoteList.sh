dca41f540de476d972397dbe8f826a05e8b11bea
Joseph Baylon <joseph@gradientx.com>
2013-12-06 11:34:46 -0800

    GX-1081: added gitRemoteList and gitShowBranch

---
+++ gitRemoteList.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+BASE_DIR=$1
+FILES=$BASE_DIR/*
+
+cd $BASE_DIR
+
+for f in $FILES
+do
+  if [ -d $f ]; then
+    echo "Processing $f file..."
+    cd $f
+    git remote -v
+    cd $BASE_DIR
+  fi
+done
